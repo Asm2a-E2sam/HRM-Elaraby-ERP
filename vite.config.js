3cd25fcbcb07987b2131eab8fe44340c51e3898b
Asmaa E Soliman <94235540+Asm2a-E2sam@users.noreply.github.com>
2024-07-23 20:13:15 +0300

    update link

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "https://hrm.elaraby-erp.net/",
+	// base: "https://hrm.elaraby-erp.net/",
+	base: "https://hrm-elaraby-front.vercel.app/",
 });
